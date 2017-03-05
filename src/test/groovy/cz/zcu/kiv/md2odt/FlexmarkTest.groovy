5be8970780810fb31b87760b91db7fb0269a2be1
Hartrik <patrikharag@gmail.com>
2017-03-05 18:24:07 +0100

    Add flexmark

---
+++ src/test/groovy/cz/zcu/kiv/md2odt/FlexmarkTest.groovy
@@ -0,0 +1,34 @@
+package cz.zcu.kiv.md2odt
+
+import com.vladsch.flexmark.ast.Node as ASTNode
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import org.junit.Test
+
+/**
+ *
+ * @version 2017-03-05
+ * @author Patrik Harag
+ */
+class FlexmarkTest {
+
+    @Test
+    void commonMark() {
+        def parser = Parser.builder().build()
+        def document = parser.parse("This is *Sparta*!")
+        def renderer = HtmlRenderer.builder().build()
+
+        assert renderer.render(document).trim() == "<p>This is <em>Sparta</em>!</p>"
+
+        printAST(document, "")
+    }
+
+    void printAST(ASTNode node, String indent) {
+        println "${indent} ${node.class} –- \"${node.chars}\""
+
+        node.children.each {
+            printAST(it, indent + " ")
+        }
+    }
+
+}

a07618c3a5b8a0b4baad02462cce49a0c48e5eb0
Josef Baloun <pepe.baloun@gmail.com>
2017-03-13 10:20:44 +0100

    added OdfSimpleWrapper

---
+++ src/main/groovy/cz/zcu/kiv/md2odt/document/odt/OdfSimpleWrapper.groovy
@@ -0,0 +1,95 @@
+package cz.zcu.kiv.md2odt.document.odt
+
+import org.odftoolkit.simple.TextDocument
+import org.odftoolkit.simple.common.navigation.TextNavigation
+import org.odftoolkit.simple.common.navigation.TextSelection
+import org.odftoolkit.simple.style.DefaultStyleHandler
+import org.odftoolkit.simple.style.StyleTypeDefinitions
+import org.odftoolkit.simple.text.Span
+import org.w3c.dom.Node
+
+/**
+ * Created by pepe on 13. 3. 2017.
+ * cookbook:
+ * http://incubator.apache.org/odftoolkit/simple/document/cookbook/index.html
+ */
+class OdfSimpleWrapper {
+    TextDocument odt
+    Node lastNode
+
+    OdfSimpleWrapper() {
+        odt = TextDocument.newTextDocument()
+        Node n = odt.getContentDom().getElementsByTagName("office:text").item(0)
+        n.removeChild(n.lastChild)      //delete an empty paragraph
+    }
+
+    OdfSimpleWrapper(File file) {
+        this.odt = TextDocument.loadDocument(file)
+    }
+
+    OdfSimpleWrapper(String documentPath) {
+        this.odt = TextDocument.loadDocument(documentPath)
+    }
+
+    void addHeading(String text, int level) {
+        if (level < 1 || level > 10)
+            throw new IllegalArgumentException("Heading level '"+level+"' is not supported.")
+
+        def paragraph = odt.addParagraph(text)
+        paragraph.applyHeading(true, level)
+
+        lastNode = odt.getContentDom().getElementsByTagName("office:text").item(0).lastChild
+        lastNode.getAttributes().getNamedItem("text:style-name").setNodeValue("Heading_20_"+level)       //set the style (1-10)
+    }
+
+    void addParagraph(String text) {
+        def paragraph = odt.addParagraph(text)
+        paragraph.setStyleName("Text_20_body")
+        lastNode = odt.getContentDom().getElementsByTagName("office:text").item(0).lastChild
+        lastNode.getAttributes().getNamedItem("text:style-name").setNodeValue("Text_20_body")
+    }
+
+    void emphasiseAll() {
+        TextNavigation nav = new TextNavigation(OdfSimpleConstan.ITALIC_MARK+"[^<]*</em>", odt)
+        TextSelection sel = null
+
+        while(nav.hasNext()) {
+            sel = nav.nextSelection()
+            sel.replaceWith(sel.getText().substring(4,sel.getText().length()-5))
+            Span sp = Span.newSpan(sel)
+            DefaultStyleHandler dsh = sp.getStyleHandler()
+            dsh.getTextPropertiesForWrite().setFontStyle(StyleTypeDefinitions.FontStyle.ITALIC)
+        }
+    }
+
+    void reEscapeAll() {
+
+        reEscape("&amp;","&")
+    }
+
+    void reEscape(String what, String with) {
+        TextNavigation nav = new TextNavigation(what, odt)
+        TextSelection sel = null
+
+        while(nav.hasNext()) {
+            sel = nav.nextSelection()
+            sel.replaceWith(with)
+        }
+    }
+
+    /** Returns the last operated Node. Used for testing.
+     *
+     * @return the last operated Node
+     * */
+    @Deprecated
+    Node getLastNode() {
+        return lastNode
+    }
+
+    void save(String documentPath) {
+        odt.save(documentPath)
+    }
+    void save(File file) {
+        odt.save(file)
+    }
+}
---
+++ src/test/groovy/cz/zcu/kiv/md2odt/document/odt/OdfSimpleWrapperTest.groovy
@@ -0,0 +1,83 @@
+package cz.zcu.kiv.md2odt.document.odt
+
+
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.w3c.dom.Node
+
+/**
+ * Created by pepe on 13. 3. 2017.
+ */
+class OdfSimpleWrapperTest {
+    OdfSimpleWrapper odt
+
+    @Before
+    void setUp() throws Exception {
+        odt = new OdfSimpleWrapper()
+    }
+
+    @Ignore
+    @Test
+    void example() throws Exception {
+        odt.addHeading("Nadpis 1",1)
+        odt.addHeading("Nadpis 10",10)
+
+        odt.addParagraph("\"   &quot;\n" +
+                "'   &apos;\n" +
+                "<   &lt;\n" +
+                ">   &gt;\n" +
+                "&   &amp;")
+
+        odt.save("test.odt")
+    }
+
+    @Ignore
+    @Test
+    void templateExample() throws Exception {
+        odt = new OdfSimpleWrapper("test.odt")
+
+        odt.addHeading("nadpis 1 se stylem sablony",1)
+        //odt.save("template_test.odt")
+    }
+
+    @Test
+    void emphasiseAllTest() throws Exception {
+        odt.addParagraph("This <em>is</em> not <em>Sparta</em>!")
+        Node parNode = odt.getLastNode()
+        //println(parNode.getTextContent())
+        odt.emphasiseAll()
+        //println(parNode.getTextContent())
+        assert parNode.getTextContent().equals("This is not Sparta!")
+        //odt.save("test.odt")
+    }
+
+    @Test
+    void escapeTest() throws Exception {
+        odt.addParagraph("This <em>is</em> not <em>Sparta</em>!")
+        Node parNode = odt.getLastNode()
+        //println(parNode.getTextContent())
+        odt.emphasiseAll()
+        //println(parNode.getTextContent())
+        assert parNode.getTextContent().equals("This is not Sparta!")
+        //odt.save("test.odt")
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    void templateNotFound1() throws Exception {
+        odt = new OdfSimpleWrapper("wrongtemplate.odt")
+    }
+    @Test(expected = FileNotFoundException.class)
+    void templateNotFound2() throws Exception {
+        odt = new OdfSimpleWrapper(new File("wrongtemplate.odt"))
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void addHeadingIllegalArgument1() throws Exception {
+        odt.addHeading("Nadpis",0)
+    }
+    @Test(expected = IllegalArgumentException.class)
+    void addHeadingIllegalArgument2() throws Exception {
+        odt.addHeading("Nadpis",11)
+    }
+}
